18bffacfe205b7972a4764ae1fde909a77c35b4b
baominxing <baominxing@foxmail.com>
2020-08-01 10:33:08 +0800

    feat : add vue learning  project

---
+++ slider_learning/src/views/slider.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex">
+    <vue-slider v-model="value"></vue-slider>
+    <vue-slider v-model="value" direction="rtl"></vue-slider>
+    <vue-slider
+      v-model="value"
+      direction="btt"
+      :height="300"
+      style="display: inline-block; margin: 30px;"
+    ></vue-slider>
+    <vue-slider
+      v-model="value"
+      direction="ttb"
+      style="display: inline-block; margin: 30px 0; height: 300px;"
+    ></vue-slider>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import $ from "jquery";
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
+export default {
+  name: "slider",
+  components: { VueSlider },
+  data() {
+    return {
+      value: [0,0],
+    };
+  },
+};
+$(function () {
+  $("#btn").on("click", function () {
+    alert("你点击了按钮");
+  });
+});
+</script>
